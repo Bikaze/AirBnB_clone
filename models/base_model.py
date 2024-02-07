741d4fd5f574d11448b7d2a4115de882c7500b69
ndiramiye2 <onlythenotes@gmail.com>
2024-02-07 11:27:34 +0200

    We gonna do this !!

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''A class that defines all common attributes/methods for other classes'''
+    def __init__(self):
+        ''' A function to initialise a unique id to a class and the time when instance is created or modified '''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''A function to print the name of the class '''
+        name = self.__class__.__name__
+        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
+
+    def save(self):
+        ''' A function to update the public attribute update_at '''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '' A function that  returns a dictionary with all keys of __dict__ of the instance '''
+        dic = self.__dict__.copy()
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
