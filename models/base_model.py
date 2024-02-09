bafcd0d864a61955fec942f3d73b08f752332dc2
bikaze <clmntmugisha@gmail.com>
2024-02-09 19:25:51 +0200

    Worked out the Create BaseModel from Dictionary feature

--- models/base_model.py
+++ models/base_model.py
@@ -5,17 +5,10 @@ from datetime import datetime
 
 class BaseModel:
     '''A class that defines all common attributes/methods for other classes'''
-<<<<<<< HEAD
-    def __init__(self):
-        ''' A function to initialise a unique id
-            to a class and the time when instance is created or modified
-        '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-=======
+
     def __init__(self, *args, **kwargs):
-        ''' A function to initialise a unique id to a class and the time when instance is created or modified '''
+        ''' A function to initialise a unique id to a class and
+            the time when instance is created or modified '''
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -26,7 +19,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
->>>>>>> bkz
 
     def __str__(self):
         '''A function to print the name of the class '''
@@ -36,9 +28,10 @@ class BaseModel:
     def save(self):
         ''' A function to update the public attribute update_at '''
         self.updated_at = datetime.now()
-            
+
     def to_dict(self):
-        ''' A function that  returns a dictionary with all keys of __dict__ of the instance'''
+        ''' A function that  returns a dictionary with all keys
+        of __dict__ of the instance'''
         dic = self.__dict__.copy()
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
