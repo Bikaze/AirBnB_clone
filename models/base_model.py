0711150f524bb17cc4ad7a13a7648222adb6db9f
ndiramiye2 <onlythenotes@gmail.com>
2024-02-09 16:32:49 +0200

    We gonna do this !!

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,9 @@ from datetime import datetime
 class BaseModel:
     '''A class that defines all common attributes/methods for other classes'''
     def __init__(self):
-        ''' A function to initialise a unique id to a class and the time when instance is created or modified '''
+        ''' A function to initialise a unique id
+            to a class and the time when instance is created or modified
+        '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -21,7 +23,9 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        ''' A function that  returns a dictionary with all keys of __dict__ of the instance '''
+        ''' A function that  returns
+            a dictionary with all keys of __dict__ of the instance
+        '''
         dic = self.__dict__.copy()
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
