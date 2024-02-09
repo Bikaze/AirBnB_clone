3944ba3222706b5bd1e51a2012fed2a8d9fcc334
bikaze <clmntmugisha@gmail.com>
2024-02-09 19:11:36 +0200

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,22 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
     '''A class that defines all common attributes/methods for other classes'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' A function to initialise a unique id to a class and the time when instance is created or modified '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''A function to print the name of the class '''
@@ -19,7 +26,7 @@ class BaseModel:
     def save(self):
         ''' A function to update the public attribute update_at '''
         self.updated_at = datetime.now()
-
+            
     def to_dict(self):
         ''' A function that  returns a dictionary with all keys of __dict__ of the instance '''
         dic = self.__dict__.copy()
