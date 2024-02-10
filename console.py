01c52f9298ed8db477ef3a39201ad7188e9a24dd
ndiramiye2 <onlythenotes@gmail.com>
2024-02-10 12:03:47 +0200

    Some Modification

--- console.py
+++ console.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+import json
+import models
+from models.engine.file_storage import storage
+
 
 """
 This program contains the entry point of the command interpreter
@@ -21,6 +25,40 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Should do nothing when Enter key is pressed"""
         pass
+
+    def de_create(self, arg):
+        '''
+            A function Creates a new instance of BaseModel,
+            saves it (to the JSON file) and prints the id
+        '''
+        if not arg:
+            print("** class name is missing **")
+            return
+
+        try:
+            class_name = arg.split[0]
+            module = __import__('models.'+class_name, fromlist=[class_name])
+            class_ = gettattr(module, class_name)
+        except (ImportError, AttributeError):
+            print("** class doesn't exist **")
+            return
+
+        instance = class_()
+        f_name = "fiile.json"
+
+        with open(f_name, 'a') as file:
+            obj_dict = instance.to_dict()
+            obj_dict["id"] = str(uuid.uuid4())
+            json.dump(obj_dict["id"]
+
+    def do_show(self, arg):
+        '''
+            A function that print the string representation of an instance
+            Usage: show <class_name> <id>
+        '''
+
+        args = arg.splint()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
