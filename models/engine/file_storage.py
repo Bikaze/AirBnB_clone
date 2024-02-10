464ebc6e3c910467f2ad1ecc7883e7587e455c93
ndiramiye2 <onlythenotes@gmail.com>
2024-02-10 21:24:03 +0200

    second round of testing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,28 +13,30 @@ class FileStorage:
         ''' A function to returns the dictionary of ids '''
         return FileStorage.__objects
 
-    def new(self):
+    def new(self, obj):
         ''' A function that the object with key of ids in __obj dictionary '''
         key ="{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
+        self.save()
 
     def save(self):
-        ''' A function to serialize __obj dictionary
-            to the JSON file by the path __file_path
-        '''
-        serialized_objects = {key: obj.to_dict() for key, obj in FileStorage.__objects
-        with open(FileStorage.__file_path, 'w') as file:
-                              json.dump(serialied_objects, file)
+        """Serialize and save the objects to the JSON file."""
+        filename = FileStorage.__file_path
+        
+        if FileStorage.__objects is not None:
+            serialized_objects = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+            
+            with open(filename, 'w', encoding='utf-8') as file:
+                json.dump(serialized_objects, file)
 
     def reload(self):
-        ''' A function that deselializes back JSON file to
-            __object. but if and only if file_path exist otherwise do nothing
-        '''
+        ''' Deserialize the JSON file back to __objects if the file exists, otherwise, do nothing. '''
         if isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    class_obj = globals()[class_name]
-                    obj_instance = class_obj(**value)
-                    FileStorage.__objects[key] = obj_instance
+                    class_obj = globals().get(class_name)
+                    if class_obj:
+                        obj_instance = class_obj(**value)
+                        FileStorage.__objects[key] = obj_instance
